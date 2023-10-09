db9f936eabc84b6ecafd25749ad2eecb0e7d2dd4
Adith Ramdas <aditramdas@gmail.com>
2023-10-09 18:30:07 +0530

    Soundsection content

--- src/components/SoundSection.jsx
+++ src/components/SoundSection.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function SoundSection() {
-  return <div>SoundSection</div>;
+  return (
+    <div className="sound-section wrapper">
+      <div className="body">
+        <div className="sound-section-content content">
+          <h2 className="title">New Sound System</h2>
+          <p className="text">Teel the base</p>
+          <span className="description">
+            From $41.62/mo. for 24 mo. or $999 before trade-in.
+          </span>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SoundSection;
